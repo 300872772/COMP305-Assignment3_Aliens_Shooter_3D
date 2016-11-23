7dc990894bafbc80f3ad6342971c051a7b1eaafe
300872772 <mamun_131@hotmail.com>
2016-11-22 23:16:09 -0500

    initial commit

---
+++ Assets/_Imported Assets/Assets_FPS_Effects/Scripts/GameController.cs
@@ -0,0 +1,132 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public Transform[]  AlienSpawnPoints;
+	public GameObject[]	Aliens;
+	public GUIText		Timer;
+	public GUIText		Score;
+	public GUIText		Multiplier;
+
+	private		int	maxAlienNumber = 10;
+	private		int	numberOfAliens;
+	private 	float	AlienSpawnTime;
+
+	private		int currentScore;
+	private		int	currentMultiplier;
+	private		float multiplierEndTime;
+	private		float multiplierDuration;
+	private		float	roundTime;
+
+	// Use this for initialization
+	void Start () {
+		AlienSpawnTime = Time.time ;
+		currentMultiplier = 1;
+		currentScore = 0;
+		multiplierDuration = 2.85f;
+
+		Score.text = currentScore.ToString();
+		roundTime = 120;
+	}
+	
+	void Update () 
+	{
+		if ( roundTime <= 0 )
+		{
+			Score.color = Color.red;
+			Timer.color = Color.grey;
+		}
+		else
+		{
+			CountAliens ();
+			UpdateScoreMultiplier ();
+
+			if ( AlienSpawnTime <= Time.time && numberOfAliens <= maxAlienNumber )
+				SpawnAlien();
+		}
+
+
+		roundTime -= Time.deltaTime;
+
+		int minutes = ((int)roundTime) / 60;
+		int seconds = ((int)roundTime) % 60;
+
+		int tens = seconds / 10;
+		int ones = seconds % 10;
+
+		//Timer.text = ((int)(roundTime)).ToString ();
+
+		if ( roundTime > 0 )
+			Timer.text = minutes + ":" + tens + ones;
+		else
+			Timer.text = "0:00";
+
+		if ( Input.GetKey( KeyCode.R ) )
+			Application.LoadLevel ( "Alien_Shooting_Game" );
+	}
+
+	void CountAliens()
+	{
+		numberOfAliens = GameObject.FindGameObjectsWithTag ("Alien1").Length;
+	}
+
+	void SpawnAlien()
+	{
+		// pick a random spawnpoint
+		int iRandomSpawn = (int)Random.Range ( 0, AlienSpawnPoints.Length );
+
+		Transform spawnPoint = AlienSpawnPoints [iRandomSpawn];
+
+		GameObject Alien = (GameObject)Instantiate (Aliens [ (int)Random.Range (0, Aliens.Length)], spawnPoint.position, spawnPoint.rotation);
+		Alien.GetComponent<Rigidbody>().AddForce( Random.insideUnitSphere * Random.Range (11100, 51100 ) );
+
+
+		AlienSpawnTime = Time.time + 1.5f; 
+	}
+
+	public void AlienDestroyed()
+	{
+		if ( roundTime <= 0 )
+			return;
+
+		currentScore += currentMultiplier;
+
+		IncreaseMultiplier ();
+
+		Score.text = currentScore.ToString ();
+	}
+
+	void	IncreaseMultiplier()
+	{
+		currentMultiplier++;
+
+		if ( currentMultiplier <= 3 )
+			multiplierEndTime = Time.time + multiplierDuration * 2.5f;
+		else if ( currentMultiplier <= 6 )
+			multiplierEndTime = Time.time + multiplierDuration * 1.7f;
+		else
+			multiplierEndTime = Time.time + multiplierDuration;
+	}
+
+	void UpdateScoreMultiplier()
+	{
+		float flTimeLeft = multiplierEndTime - Time.time;
+
+		if ( flTimeLeft <= 0 )
+		{
+			Multiplier.enabled = false;
+			currentMultiplier = 1;
+		}
+		else
+		{
+			Multiplier.enabled = true;
+			Multiplier.text = currentMultiplier.ToString() + "x";
+
+			Color newColor = Color.green;
+			newColor.a = ( flTimeLeft / multiplierDuration );
+
+			Multiplier.color = newColor;
+		}
+	}
+}
---
+++ Assets/_Scripts/PlayerShooting.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlayerShooting : MonoBehaviour {
+
+	// PUBLIC VARIABLES FOR TESTING
+	public Transform FlashPoint;
+	public GameObject MuzzleFlash;
+	public GameObject Explosion;
+	public GameObject BulletImpact;
+	public AudioSource RifleShotSound;
+	public Transform PlayerCam;
+
+
+
+
+	// PRIVATE VARIABLES
+	private GameObject _gameControllerObject;
+	private GameControllerScore _gameControllerScore;
+
+
+	// Use this for initialization
+	void Start () {
+
+		this._gameControllerObject = GameObject.Find ("GameControllerScore");
+		this._gameControllerScore = this._gameControllerObject.GetComponent<GameControllerScore> () as GameControllerScore;
+	}
+	
+	// Update is called once per frame (for Physics)
+	void FixedUpdate () {
+		if (Input.GetButtonDown ("Fire1")) {
+			// show the MuzzleFlash at the FlashPoint without any rotation
+			Instantiate (this.MuzzleFlash, this.FlashPoint.position, Quaternion.identity);
+
+			// need a variable to hold the location of our Raycast Hit
+			RaycastHit hit;
+
+			// if raycast hits an object then do something...
+			if (Physics.Raycast (this.PlayerCam.position, this.PlayerCam.forward, out hit)) {
+
+				if (hit.transform.gameObject.CompareTag ("Alien")) {
+					Instantiate (this.Explosion, hit.point, Quaternion.identity);
+					this._gameControllerScore.ScoreValue = this._gameControllerScore.ScoreValue + 10;
+					Destroy (hit.transform.gameObject);
+				} else {
+					Instantiate (this.BulletImpact, hit.point, Quaternion.identity);
+				}
+
+
+			}
+
+			// Play Rifle Sound
+			this.RifleShotSound.Play();
+		}
+	}
+	void OnCollision(Collider other){
+		
+
+	}
+    //if I touch anything it will transport me to another scene
+    void OnTriggerEnter(Collider other){
+       // 
+		if (other.gameObject.CompareTag ("Exit")) {
+			SceneManager.LoadScene("OutDoor");
+		}
+		if (other.gameObject.CompareTag ("Alien")) {
+			this._gameControllerScore.LivesValue = this._gameControllerScore.LivesValue - 1;
+		}
+    }
+}
